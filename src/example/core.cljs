e3759f73ec90cbab1ac6ddaeac721cdc5029e5c1
Mikkel Gravgaard <mikkel@klokke.dk>
2020-01-31 15:31:17 +0100

    initial commit

---
+++ src/example/core.cljs
@@ -0,0 +1,11 @@
+(ns example.core
+  (:require [reagent.core :as r]))
+
+(defonce !app-state (r/atom nil))
+
+(defn app []
+  (let [{:keys [message]} @!app-state]
+    [:p (or message "hello")]))
+
+(defn ^:dev/after-load main []
+  (r/render [app] (js/document.getElementById "app")))
