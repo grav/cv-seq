65cc0b8ddbbeb769e6bebac698fbc5c0ee23b6ad
Mikkel Gravgaard <mikkel@klokke.dk>
2020-03-01 13:56:12 +0100

    wip

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -1,6 +1,5 @@
 (ns example.core
-  (:require [reagent.core :as r]
-            [idb]))
+  (:require [reagent.core :as r]))
 
 (defonce !app-state (r/atom nil))
 
@@ -8,17 +7,6 @@
   (let [{:keys [message]} @!app-state]
     [:p (or message "hello")]))
 
-; (defn upgrade-db [db]
-;   (println 'upgrade-db)
-;   (.createObjectStore db "my-store2"
-;                       #js {"keyPath" "id"}))
-
-; (defn put [m]
-;   (-> (.open idb "my-db" 1 upgrade-db)
-;       (.then (fn [db]
-;                (let [tx (.transaction db "my-store2" "readwrite")]
-;                  (-> tx (.objectStore "my-store2") (.put (clj->js m)))
-;                  (.-complete tx))))))
 
 (defn ^:dev/after-load main []
   (r/render [app] (js/document.getElementById "app")))
