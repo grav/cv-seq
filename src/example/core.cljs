212842eabecc551e88f6edca000f3ae17a384edd
Mikkel Gravgaard <mikkel@klokke.dk>
2020-03-01 17:49:57 +0100

    wip

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -1,12 +1,15 @@
 (ns example.core
   (:require [reagent.core :as r]
-            [example.view :as v]))
+            [example.view :as v]
+            [example.seq :as seq]))
 
 (def n-steps 10)
 
-(defonce !app-state (r/atom {:steps (vec (repeat n-steps 0))}))
+(defonce !steps (r/atom (vec (repeat n-steps 0))))
 
 (defonce !ctx (atom nil))
+
+(defonce !app-state (r/atom nil))
 
 (defn  setup! []
   (println "setup")
@@ -29,19 +32,38 @@
     (.start osc)))
 
 (defn app []
-  (let [{:keys [steps]} @!app-state]
-    [:div
-     (if-let [_ctx @!ctx]
-       (do
-         (setup!)
-         [v/sequence-view {:steps steps
-                           :step-playing 2
-                           :on-steps-changed #(swap! !app-state assoc :steps %)}])
-       [:button {:on-click (fn []
-                             (setup!))} "Start"])]))
+  [:div
+   (if-let [_ctx @!ctx]
+     (do
+       (setup!)
+       [:div
+        [:button {:on-click #(seq/play-repeatedly
+                               {:now-fn (fn [] (.now (.-performance js/window)))
+                                :on-update-beat (fn [%]
+                                                  (swap! !app-state assoc :beat %))
+                                :seq-transform (fn [seq]
+                                                 [{:device nil
+                                                   :sequence {:sequence (for [s seq]
+                                                                          [{:note s}])}}])
+                                :!sequences !steps}
+                               {:beat 0
+                                :time 0})} "Start"]
+        [v/sequence-view {:steps @!steps
+                          :step-playing 2
+                          :on-steps-changed #(reset! !steps %)}]])
+     [:button {:on-click (fn []
+                           (setup!))}
+      "Start"])])
+
+
 
 (defn ^:dev/after-load main []
 
   (r/render [app] (js/document.getElementById "app")))
+
+
+
+
+
 
 
---
+++ src/example/seq.cljs
@@ -0,0 +1,66 @@
+(ns example.seq)
+
+(defn secs-per-tick
+  [bpm]
+  (/ (/ 1 (/ bpm 60)) 4))
+
+(defn- next-notes [latency {:keys [sequence transpose] :or {transpose 0}} beat now time spt]
+  (->> sequence
+       ;; TODO - doesn't belong here - it's for transposing?
+       (map (fn [notes] (->> notes
+                             (map (fn [{:keys [note]
+                                        :as   v}]
+                                    (assoc v :note (+ transpose note)))))))
+       (repeat)
+       (apply concat)
+       (drop (mod beat (count sequence)))
+       (map-indexed (fn [i v] [(+ time (* i spt)) v]))
+       (take-while (fn [[t _]] (< t (+ now (* 1.5 latency)))))))
+
+(defn play-sequences! [{:keys [latency bpm sequences now beat time]}]
+  ":notes - notes to be immediately queued up
+   :beat :time - pointers to next "
+  (let [spt (secs-per-tick bpm)
+        sequences (for [{:keys [device sequence]} sequences
+                        :when (:sequence sequence)]
+                    {:device  device
+                     :notes   (next-notes latency sequence beat now time spt)
+                     :channel (or (:channel sequence) 0)})
+
+        diff (- now time)
+        c (max (int (/ diff spt)) (or (->> (map count (map :notes sequences))
+                                           (apply max))
+                                      0))
+        beat' (+ c beat)
+        time' (+ (* spt c) time)]
+    {:beat      beat'
+     :time      time'
+     :sequences sequences}))
+
+(def latency 1)
+
+(defn ding [args]
+  (print 'ding args))
+
+(defn play-repeatedly
+  [{:keys [now-fn on-update-beat !sequences seq-transform]} {:keys [beat time]}]
+  (let [bpm 110
+        {:keys [position sequences]
+         :as res} (play-sequences! {:latency latency
+                                    :bpm bpm
+                                    :sequences (seq-transform @!sequences)
+                                    :now (/ (now-fn) 1000)
+                                    :beat beat
+                                    :time time})]
+    (doseq [n (for [{:keys [device notes channel]} sequences
+                    [i vs] notes
+                    {:keys [note sustain]} vs]
+                [device channel (+ 0x24 note) i sustain])]
+      (apply ding n))
+    (on-update-beat beat)
+    (js/setTimeout #(play-repeatedly {:now-fn now-fn
+                                      :!sequences !sequences
+                                      :seq-transform seq-transform
+                                      :on-update-beat on-update-beat}
+                                     res)
+                   (* latency 1000))))
