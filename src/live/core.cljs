3b01585524cc78609caa8114b8790cc0f18b6ba9
Mikkel Gravgaard <mikkel@klokke.dk>
2020-10-08 22:39:47 +0200

    wip

--- src/live/core.cljs
+++ src/live/core.cljs
@@ -1,6 +1,7 @@
 (ns live.core
   (:require [clojure.string :as str]
-            [cljs-bean.core :refer [bean]]))
+            [cljs-bean.core :refer [bean]]
+            [clojure.pprint]))
 
 (def output-name-prefix "Synth input port")
 
@@ -15,7 +16,7 @@
 
 (defonce origin (.now js/performance))
 
-(defn get-output-p []
+(defn get-output-p [prefix]
   (-> (.requestMIDIAccess js/navigator)
       (.then (fn [m]
                (->> m
@@ -23,7 +24,7 @@
                     (.values)
                     es6-iterator-seq
                     (filter #(str/starts-with?
-                               (.-name %) output-name-prefix))
+                               (.-name %) prefix))
                     first)))))
 
 (defn duration []
@@ -83,7 +84,7 @@
     [{:type :note-on
       :time offset
       :data1 note-val
-      :data2 (int (* (or velocity 0.5) 128))
+      :data2 (int (* (or velocity 0.8) 128))
       :channel channel}
      {:type :note-off
       :time (+ offset
@@ -139,7 +140,6 @@
         delay (max 0 (- (* 1000 offset) now (* 1000 latency)))
         next-offset (+ offset
                        (* (bpm->secs-per-beat tempo) loop-length))]
-    (println 'delay delay)
     (js/setTimeout (fn []
                      (play-notes! args
                                   (notes-fn))
@@ -149,24 +149,24 @@
                    delay)))
 
 (defn funk []
-  (->> [nil nil nil nil :d1 nil nil nil]
-       (repeat 2)
+  (->> (fn [] [nil nil nil nil :d1 nil nil (rand-nth [nil :d1])])
+       (repeatedly 2)
        (apply concat)))
 
 
 (defn funk-clap []
   (->> [[nil nil nil :d#1 nil nil :d#1 nil]
-        [nil :d#1 nil :d#1 nil nil :d#1 nil]]
+        #_[nil :d#1 nil :d#1 nil nil :d#1 nil]]
        rand-nth
        (repeat 2)
        (apply concat)))
 
 (defn funk-clap2 []
-  (->> [[nil nil :d#1 :d#1 nil nil :d#1 nil]
-        [:d#1 :d#1 nil :d#1 nil nil :d#1 nil]]
-       rand-nth
-       (repeat 2)
-       (apply concat)))
+  [] #_(->> [[nil nil :d#1 :d#1 nil nil :d#1 nil]
+             [:d#1 :d#1 nil :d#1 nil nil :d#1 nil]]
+            rand-nth
+            (repeat 2)
+            (apply concat)))
 
 (defn bd []
   (->> [:c1 nil nil nil]
@@ -174,13 +174,21 @@
        (apply concat)))
 
 (defn hihats []
-  []#_ (->> [:f#1 :f#1 :a#1 :f#1]
-            (repeat 4)
-            (apply concat)))
+  (->> (fn [] [:f#1 :f#1 (rand-nth [:f#1 :a#1]) :f#1])
+       (repeatedly 4)
+       (apply concat)))
 
 (defn bass []
-  (apply concat (repeat 2 [:c1 :g1 nil :d#2 nil nil :c1 nil])))
+  (apply concat (repeat 2 [:c1 :c1 nil :c1 nil nil :c1 nil])))
 
+(defn chord1 []
+  [:d#5 nil nil :d#5 nil nil :d#5 nil nil :d#5 nil nil :d#5 nil :f5 nil])
+
+(defn chord2 []
+  [:g4 nil nil :g4 nil nil :g4 nil nil :g4 nil nil :g4 nil :a#4 nil])
+
+(defn chord3 []
+  [:c5 nil nil :c5 nil nil :c5 nil nil :c5 nil nil :c5 nil :c5 nil])
 
 (defn my-notes []
 
@@ -198,12 +206,12 @@
   (loop! {:offset (next-bar 120 4)
           :tempo 120
           :loop-length 4
-          :channel 0}
-         #'my-notes))
+          :channel 9}
+         #'funk))
 
 ;;; fluidsynth
 ;;; fluidsynth -a pulseaudio -m alsa_seq -l /usr/share/soundfonts/freepats-general-midi.sf2
 
 (defn ^:export init []
-  (-> (get-output-p)
+  (-> (get-output-p "TORAIZ")
       (.then #(swap! !app-state assoc :output %))))
