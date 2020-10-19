e95aab279833d8494b213685f9d43331bb8c8de5
Mikkel Gravgaard <mikkel@klokke.dk>
2020-10-19 22:45:42 +0200

    wip

--- src/live/ses1.cljs
+++ src/live/ses1.cljs
@@ -11,32 +11,35 @@
        (repeat 4)
        (apply concat)))
 
+(defn perc1 []
+  (->> [:a#1 nil :g1 nil :c1 nil nil :c1 nil nil :c2 nil nil nil :c3]))
+
 (defn bassic1
   []
   (->> [:d0 nil :d1 :d1 :d1 :d1 :d2 :d2 nil nil nil :d1 nil nil :d1]))
 
 (defn bassic2
   []
-  (->> [nil nil nil nil :c1 nil nil :c1 nil :c1]))
+  (->> [nil nil nil nil :c2 nil nil :c2 nil :c2]))
+
+(defn bassic3 []
+  [nil nil :d2 nil :d2 nil nil nil nil :f3 nil nil :d2 nil nil :d3])
+
 
 (defn hh1 []
-  (->> (fn [] [nil nil :f#2 (when (> (rand) 0.5)
-                              :f#2)])
+  (->> (fn [] [nil nil :f#1 (when (> (rand) 0.5)
+                              :f#1)])
        (repeatedly 4)
        (apply concat)))
 
 (defn sn1 []
-  (->> [nil nil nil nil :c1 nil nil nil ]
-       (repeat 2)
-       (apply concat)))
-
-(defn snare1 []
-  (->> [nil nil nil nil :c1 nil nil nil]
+  (->> [nil nil (when (> (rand) 0.2) :d2) nil :d1 nil nil (when (> (rand) 0.7)
+                                                           :d2)]
        (repeat 2)
        (apply concat)))
 
 (defn arp1 []
-  (->> [:c2 :f2 :g2 :a#2 :c3 :c2 :c3 :a#1]
+  (->> [:c3 :f3 :g3 :a#3 :c4 :c3 :c4 :a#2]
        (repeat 2)
        (apply concat)))
 
@@ -47,5 +50,5 @@
   (loop! {:offset (next-bar 120 4)
           :tempo 120
           :loop-length 4
-          :channel 2}
-         #'chords1))
+          :channel 0}
+         #'perc1))
